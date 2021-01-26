3999d4909089004df306c05403dd644273ceccf6
Sevara Isroilova <sevara.i.d@gmail.com>
2021-01-26 15:23:30 -0500

    Sevara4 commit 5

--- src/test/java/HelloTest2.java
+++ src/test/java/HelloTest2.java
@@ -1,6 +1,6 @@
 public class HelloTest2 {
     public static void main(String[] args) {
-        System.out.println("More ");
+        System.out.println("Sevara");
         System.out.println("test 2");
         System.out.println("test 34");
     }
