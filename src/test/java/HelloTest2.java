cfd52690b8f1c8c052bff7577a03e6a27d0652a7
Sevara Isroilova <sevara.i.d@gmail.com>
2021-01-26 14:22:47 -0500

    hello7

--- src/test/java/HelloTest2.java
+++ src/test/java/HelloTest2.java
@@ -2,6 +2,7 @@ public class HelloTest2 {
     public static void main(String[] args) {
         System.out.println("More ");
         System.out.println("test 2");
+        System.out.println("test 3");
     }
 }
 
