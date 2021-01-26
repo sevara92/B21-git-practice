bc141a3f709883df825e23bd80877b305ebcdf83
Sevara Isroilova <74113791+sevara92@users.noreply.github.com>
2021-01-26 16:38:51 -0500

    HelloTest
    beautiful

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,6 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("HelloTest 11111");
         System.out.println("Second line ");
-        System.out.println("tired");
+        System.out.println("YOu are not tired");
+        
     }
 }
