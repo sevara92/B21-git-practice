e889f62ec20f7f47989545adc794cad10a6d8d87
Sevara Isroilova <sevara.i.d@gmail.com>
2021-01-26 15:27:38 -0500

    Sevara4 commit 7 messed up everything commited pushed from master

--- src/test/java/Sevara4.java
+++ src/test/java/Sevara4.java
@@ -2,5 +2,6 @@ public class Sevara4 {
     public static void main(String[] args) {
         System.out.println("Commit");
         System.out.println("Tired of making commits");
+        System.out.println("messed up everything");
     }
 }
